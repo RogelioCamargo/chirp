7091caec01a2d261e7d90601ce5b9e79235ae4e9
Rogelio Camargo <rogelio.camargo2016@gmail.com>
2023-04-14 07:25:55 -0700

    Create posts router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,8 +6,10 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  
   const user = useUser();
+
+	const { data } = api.posts.getAll.useQuery();
 
   return (
     <>
@@ -21,6 +23,17 @@ const Home: NextPage = () => {
           {!user.isSignedIn && <SignInButton />}
           {!!user.isSignedIn && <SignOutButton />}
 			  </div>
+				<div>
+					{data?.map((post) => (
+						<div key={post.id}>
+							
+								{post.content}
+							
+
+							</div>
+					))}
+
+				</div>
       </main>
     </>
   );
