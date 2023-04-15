52c17a3cd4379698d541f76226e3a84d42987ace
Rogelio Camargo <rogelio.camargo2016@gmail.com>
2023-04-14 22:39:08 -0700

    Ratelimit the post creation endpoint

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -17,13 +17,23 @@ const filterUserForClient = (user: User) => {
   };
 };
 
+import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
+import { Redis } from "@upstash/redis";
+
+// Create a new ratelimiter, that allows 3 requests per 1 minute
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(3, "1 m"),
+  analytics: true,
+});
+
 export const postsRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
     const posts = await ctx.prisma.post.findMany({
       take: 100,
-			orderBy: {
-				createdAt: 'desc',
-			}
+      orderBy: {
+        createdAt: "desc",
+      },
     });
 
     const users = (
@@ -57,6 +67,14 @@ export const postsRouter = createTRPCRouter({
     .input(z.object({ content: z.string().emoji().min(1).max(280) }))
     .mutation(async ({ ctx, input }) => {
       const authorId = ctx.userId;
+
+      const { success } = await ratelimit.limit(authorId);
+
+      if (!success) {
+        throw new TRPCError({
+          code: "TOO_MANY_REQUESTS",
+        });
+      }
 
       const post = await ctx.prisma.post.create({
         data: {
